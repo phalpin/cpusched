a7b728c891f812c30a55315efffda2f09a0d5432
Phillip Halpin <p.d.halpin@gmail.com>
2013-10-07 02:11:01 -0400

    Basis for RR and SJF
    Added basis for RR and SJF queues. Just need code now!

---
+++ cpusched/Queues/RR.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpusched.Queues
+{
+    /// <summary>
+    /// Round Robin Queue.
+    /// </summary>
+    class RR : ProcessQueue
+    {
+        private int _timequantum = 0;
+        
+        /// <summary>
+        /// The Time Quantum for this Round Robin queue.
+        /// </summary>
+        public int TimeQuantum
+        {
+            get { return this._timequantum; }
+            set { this._timequantum = value; }
+        }
+
+        /// <summary>
+        /// Sorting routine for Round Robin
+        /// </summary>
+        protected override void Sort()
+        {
+            //Do stuff
+        }
+
+    }
+}
---
+++ cpusched/Queues/SJF.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpusched.Queues
+{
+    /// <summary>
+    /// Shortest Job First Queue.
+    /// </summary>
+    class SJF : ProcessQueue
+    {
+
+        /// <summary>
+        /// Sorting routine for SJF
+        /// </summary>
+        protected override void Sort()
+        {
+            //Do stuff
+        }
+
+    }
+}
