7bb8aa13dba618921d49965e674e6ca0dfbd9c16
richierichrawr <jan2011cl@gmail.com>
2013-10-15 18:50:54 -0400

    SJF without context switch
    not checking for when proc hits I/O.  Very close to actual solution.
    Committed just in case a context switch could later be done
    independently

--- cpusched/Queues/SJF.cs
+++ cpusched/Queues/SJF.cs
@@ -18,6 +18,42 @@ namespace cpusched.Queues
         protected override void Sort()
         {
             //Do stuff
+            if (this._readyprocs.Count == 0 && this._ioprocs.Count == 0)
+            {
+                this._state = QueueState.COMPLETE;
+            }
+            else
+            {
+                //If there's no processes ready, but processes in the IO queue, this state changes to allIO.
+                if (this._readyprocs.Count == 0 && this._ioprocs.Count > 0)
+                {
+                    this._state = QueueState.ALLIO;
+                    this._next = null;
+                }
+                //Otherwise, if there's processes in the ready queue, this queue is ready.
+                else if (this._readyprocs.Count > 0)
+                {
+                    this._state = QueueState.READY;
+
+                    Processes.Process sjf = this._readyprocs[0];
+
+                    foreach (Processes.Process p in this._readyprocs)
+                    {
+                        if (p.Time.Current.Duration < sjf.Time.Current.Duration)
+                        {
+                            sjf = p;
+                        }
+                        else {this._next = sjf; }
+
+                        if (this._totalTime == 265)
+                        {
+                            object r = new object();
+                        }
+                    }
+                    this._next = sjf;
+                }
+                
+            }
         }
 
     }
